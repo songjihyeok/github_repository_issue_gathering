a9bbab5b3d196890db1a4bb6668f26a793aacd62
songjihyeok <porr3444@naver.com>
2023-01-30 21:33:07 +0900

    liked 기능 추가 완료

--- src/component/home/list/index.tsx
+++ src/component/home/list/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
+import React from 'react'
 import Card from "../Card"
 import { itemInCludeLikedInterface } from '@/pages/Home/type'
 
@@ -8,9 +7,19 @@ export default function List({ searchResultList, setSearchResultList }: {
      searchResultList: itemInCludeLikedInterface[],
      setSearchResultList: React.Dispatch<React.SetStateAction<itemInCludeLikedInterface[]>>
 }) {
+     const localLikedIdList = localStorage.getItem("likedList") ?? JSON.stringify({ data: [] })
+     const likedIdList = JSON.parse(localLikedIdList).data ?? []
+     const likedCheckedSearchResultList = searchResultList.map((element) => {
+          if (likedIdList.includes(element.id)) {
+               return {
+                    ...element,
+                    liked: true
+               }
+          }
+          return element
+     })
 
      const likeHandler = (element: itemInCludeLikedInterface) => {
-
           const likeHandledResultList = searchResultList.map((item) => {
                if (item.id === element.id) {
                     return {
@@ -22,17 +31,32 @@ export default function List({ searchResultList, setSearchResultList }: {
                }
           })
           setSearchResultList(likeHandledResultList)
+          setLocalStorage(element.id)
      }
 
-
-
-
-
+     const setLocalStorage = (id: number) => {
+          const localLikedIdList = localStorage.getItem("likedList") ?? JSON.stringify({ data: [] })
+          const gotLikedIdList = JSON.parse(localLikedIdList)?.data ?? []
+          if (gotLikedIdList.includes(id)) {
+               const removedLikedList = JSON.stringify(
+                    {
+                         data: gotLikedIdList
+                              .filter((element: number) => element !== id)
+                    })
+               return localStorage.setItem("likedList", removedLikedList)
+          } else {
+               const updatedLikedList = JSON.stringify(
+                    {
+                         data: [...gotLikedIdList, id]
+                    })
+               return localStorage.setItem("likedList", updatedLikedList)
+          }
+     }
 
 
      return (
           <>
-               {searchResultList.map((element: itemInCludeLikedInterface, index: number) => {
+               {likedCheckedSearchResultList.map((element: itemInCludeLikedInterface, index: number) => {
                     return <Card key={index}
                          item={element}
                          likeHandler={likeHandler}
